bd17c7170f00e017ea9eb4178b97b1a6fe99293d
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2022-04-20 10:41:20 +0300

    HW4

---
+++ complex.h
@@ -0,0 +1,13 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+double ChetN(double a, double b);
+double ChetM(double a, double b, double n);
+
+double tg(double a);
+
+#endif // !COMPLEX_H
+
